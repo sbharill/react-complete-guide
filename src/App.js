95262e35d8a0c76fa6bb076f0a07a4ca4fa7c935
Shobhan B <shobhan.bharill@gmail.com>
2019-05-05 19:56:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component{
 
   render(){
     const style = {
-      backgroundColor: "blue",
+      backgroundColor: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
